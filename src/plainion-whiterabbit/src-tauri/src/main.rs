15890e8628bf017bb7ace847eadd9c6ea3badc17
plainionist <mail@plainionist.net>
2024-11-04 12:45:37 +0100

    tauri backend (rust) simplified

--- src/plainion-whiterabbit/src-tauri/src/main.rs
+++ src/plainion-whiterabbit/src-tauri/src/main.rs
@@ -1,6 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_dotnet_bridge_host;
+
+#[tauri::command]
+fn dotnet_request(request: &str) -> String {
+    tauri_dotnet_bridge_host::process_request(request)
+}
+
 fn main() {
-    plainion_whiterabbit_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![dotnet_request])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
