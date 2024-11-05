957b0e7d287a5b0e24c3597fe1f4b364b0e188a6
plainionist <mail@plainionist.net>
2024-11-05 19:18:28 +0100

    date selection fixed

--- src/plainion-whiterabbit/src/components/DatePicker.vue
+++ src/plainion-whiterabbit/src/components/DatePicker.vue
@@ -1,13 +1,9 @@
 <template>
-  <DatePicker
-    v-model="selectedDate"
-    @update="onDateChange"
-    class="border border-gray-300 px-2 py-1 rounded focus:outline-none focus:border-blue-500 w-32"
-  />
+  <DatePicker v-model="selectedDate" class="border border-gray-300 px-2 py-1 rounded focus:outline-none focus:border-blue-500 w-32" />
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref, onMounted } from 'vue'
+  import { defineComponent, ref, onMounted, watch } from 'vue'
   import DatePicker from 'vue3-datepicker'
   import { emit } from '@tauri-apps/api/event'
 
@@ -16,9 +12,12 @@
     setup() {
       const selectedDate = ref(new Date())
 
+      watch(selectedDate, () => {
+        onDateChange()
+      })
+
       async function onDateChange() {
         emit('date-selected', selectedDate.value)
-        console.log('date selected')
       }
 
       onMounted(() => {
