236c0f414cdd6bca42ea98022078d0452b6466ac
plainionist <mail@plainionist.net>
2024-11-05 13:27:40 +0100

    TauriDotNetBridge updated to latest

--- src/plainion-whiterabbit/src-tauri/build.rs
+++ src/plainion-whiterabbit/src-tauri/build.rs
@@ -1,3 +1,11 @@
+use std::process::Command;
+
 fn main() {
+    Command::new("dotnet")
+        .arg("build")
+        .current_dir("../src-dotnet")
+        .status()
+        .expect("Failed to run dotnet build");
+
     tauri_build::build()
 }
