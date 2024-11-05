37bb1f2eb8099d6e52c8d3b2f40b62726e3b0c55
plainionist <mail@plainionist.net>
2024-11-05 15:05:37 +0100

    date picker fixed

--- src/plainion-whiterabbit/src/components/DatePicker.vue
+++ src/plainion-whiterabbit/src/components/DatePicker.vue
@@ -1,5 +1,6 @@
 <template>
-  <vue3-datepicker v-model="selectedDate" @update="onDateChange" />
+  <DatePicker v-model="selectedDate" @update="onDateChange" 
+    class="border border-gray-300 px-2 py-1 rounded focus:outline-none focus:border-blue-500 w-32"/>
 </template>
 
 <script lang="ts">
