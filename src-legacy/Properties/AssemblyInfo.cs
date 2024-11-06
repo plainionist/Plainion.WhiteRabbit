877f9378d868fcc0a8726310be539d0995708f6e
plainionist <mail@plainionist.net>
2024-11-06 16:08:12 +0100

    dead legacy files removed

--- src-legacy/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Plainion.WhiteRabbit")]
-[assembly: AssemblyProduct("Plainion.WhiteRabbit")]
-[assembly: AssemblyDescription("Plainion.WhiteRabbit")]
-[assembly: AssemblyCopyright("Copyright @ 2024")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
