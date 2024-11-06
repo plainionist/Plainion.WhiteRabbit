d0d445cc696a5b77fdaff167aab1f8021b761dbd
plainionist <mail@plainionist.net>
2024-11-06 12:47:27 +0100

    packaging cleaned up

--- src/plainion-whiterabbit/src-tauri/build.rs
+++ src/plainion-whiterabbit/src-tauri/build.rs
@@ -1,13 +1,3 @@
-use std::process::Command;
-
 fn main() {
-    println!("cargo:rerun-if-changed=../src-dotnet");
-
-    Command::new("dotnet")
-        .arg("build")
-        .current_dir("../src-dotnet")
-        .status()
-        .expect("Failed to run dotnet build");
-
     tauri_build::build()
 }
