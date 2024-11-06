934ef4ab45556f3deb5ab0d12780e16eb8464717
plainionist <mail@plainionist.net>
2024-11-06 16:40:31 +0100

    legacy dead code removed

--- src-legacy/Controller/Channel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Plainion.WhiteRabbit.Presentation
-{
-    public class Channel
-    {
-        public Action<TimeSpan> OnTimeElapsedChanged { get; set; }
-    }
-}
--- src-legacy/View/ViewFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Plainion.WhiteRabbit.View
-{
-    internal class ViewFactory
-    {
-        internal static IView CreateMainView(IController controller) => new MainUI(controller);
-
-        internal static IView CreateTimerView(IController controller) => new SlimForm(controller);
-    }
-}
