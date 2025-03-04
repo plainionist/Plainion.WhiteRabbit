9b01a04c8be98dd5683f882a581e8abe41b06e43
plainionist <mail@plainionist.net>
2025-03-04 09:13:48 +0100

    auto commit added

--- src-dotnet/WhiteRabbit.TauriPlugIn/DataStore.cs
+++ src-dotnet/WhiteRabbit.TauriPlugIn/DataStore.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Newtonsoft.Json;
 
 namespace WhiteRabbit.TauriPlugIn;
@@ -25,6 +26,37 @@ public class DataStore
     private static string GetFile(DateTime date) =>
         Path.Combine(DBStore, $"{date:yyyy-MM-dd}.json");
 
-    public void Update(DateTime date, IEnumerable<Activity> activities) =>
+    public void Update(DateTime date, IEnumerable<Activity> activities)
+    {
         File.WriteAllText(GetFile(date), JsonConvert.SerializeObject(activities, Formatting.Indented));
+
+        CommitOnDemand();
+    }
+
+    private void CommitOnDemand()
+    {
+        if (!Directory.Exists(Path.Combine(DBStore, ".git")))
+        {
+            return;
+        }
+
+        var git = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "git",
+                Arguments = "commit -am '.'",
+                WorkingDirectory = DBStore,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            }
+        };
+
+        git.Start();
+
+        // don' block the UI
+        //git.WaitForExit();
+    }
 }
